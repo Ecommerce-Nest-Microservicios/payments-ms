487548d4b65a0f59785b6ffcc0c9f152bec63cde
NELSON ANDRES LUENGAS SILVA <90053867+NELSONLUENGAS@users.noreply.github.com>
2025-03-18 17:20:41 -0500

    init

--- src/main.ts
+++ src/main.ts
@@ -33,6 +33,7 @@ async function bootstrap() {
   await app.startAllMicroservices();
   await app.listen(process.env.PORT ?? 3003);
 
+  console.log('payments-ms');
   logger.log(`Payments Microservice running...`);
 }
 bootstrap();
